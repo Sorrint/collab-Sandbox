5cca8bc8b52a535b4d5b4e79d03a2aafd33fb696
Anton Lebedev <sorrint@gmail.com>
2022-07-09 13:36:20 +0800

    rewrite sound module, some fixes with functional

---
+++ src/modules/context-menu/assets/soundData.js
@@ -0,0 +1,7 @@
+export const soundData = [
+  { trackName: 'Mortal Combat Theme' },
+  { trackName: 'The Pink Panther Theme' },
+  { trackName: 'The World Is Not Enough (Agent 007 Theme' },
+  { trackName: "OST Chip 'n' Dale: Rescue Rangers" },
+  { trackName: 'Mission: Impossible Theme' },
+];
